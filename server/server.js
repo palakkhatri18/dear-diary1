ade5f4a907c67efc16f324fc61a07d17e3e7ccf8
Palak Khatri <palakkhatri934@gmail.com>
2025-07-17 14:42:23 +0530

    fix: remove localhost reference for Render

--- server/server.js
+++ server/server.js
@@ -24,5 +24,5 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
