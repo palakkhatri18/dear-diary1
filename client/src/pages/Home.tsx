5956bbd2d2018f447135664789f2ec31115cb0fa
Palak Khatri <palakkhatri934@gmail.com>
2025-07-31 23:55:51 +0530

    small

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,11 +8,11 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center justify-start p-4 sm:p-8">
-      <img 
-        src={welcomeImage} 
-        alt="Welcome to your diary, start journaling today!"
-        className="w-full max-w-lg mb-8" 
-      />
+        <img 
+    src={welcomeImage} 
+    alt="Welcome to your diary, start journaling today!"
+    className="w-full max-w-md mb-4" 
+        />
       <div className="calendar-3d-wrapper">
         <CalendarComponent />
       </div>
